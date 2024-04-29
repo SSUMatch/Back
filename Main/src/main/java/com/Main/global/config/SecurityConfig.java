8fe66824857eeb272d4760255b6b3bd5d221d8c4
Bae Hyeonseo <haulqogustj@naver.com>
2024-04-29 16:25:26 +0900

    [Main]: cors오류 설정

---
+++ Main/src/main/java/com/Main/global/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.Main.global.config;
+
+
+import com.kusitms29.backendH.infra.config.auth.ExceptionHandlerFilter;
+import com.kusitms29.backendH.infra.config.auth.JwtAuthenticationEntryPoint;
+import com.kusitms29.backendH.infra.config.auth.JwtAuthenticationFilter;
+import com.kusitms29.backendH.infra.config.auth.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final CorsConfig corsConfig;
+    // TODO api 추가될 때 white list url 확인해서 추가하기.
+
+    private static final String[] whiteList = {"/**"};
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(whiteList);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+                .addFilter(corsConfig.corsFilter())
+                .build();
+    }
+}
