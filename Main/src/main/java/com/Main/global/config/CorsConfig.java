2f825dac6b3e5cf0f885f198efe87eb6f3a23123
Bae Hyeonseo <haulqogustj@naver.com>
2024-06-05 11:39:27 +0900

    [feat]:config

--- Main/src/main/java/com/Main/global/config/CorsConfig.java
+++ Main/src/main/java/com/Main/global/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("");
+        config.addAllowedOrigin("https://match-xi.vercel.app/");
         config.addAllowedHeader("*");
         config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         source.registerCorsConfiguration("/**",config);
