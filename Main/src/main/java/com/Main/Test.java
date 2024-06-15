7389b98781c2bb13ffe0ffd609bfe6519e688203
Bae Hyeonseo <haulqogustj@naver.com>
2024-06-15 15:40:02 +0900

    [Main]:테스트

--- Main/src/main/java/com/Main/Test.java
+++ Main/src/main/java/com/Main/Test.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
     @GetMapping("/test")
     String test(){
-            return"hello";
+            return"capstone";
         }
 
 }
